790638b692a14c94ccdadc0f387243148b58d115
ProgramHunter2021 <110998991+ProgramHunter2021@users.noreply.github.com>
2023-10-09 14:18:15 +0800

    Add files via upload

--- booking/utils.py
+++ booking/utils.py
@@ -22,6 +22,8 @@ class MyDateTime:
     # 取得天數
     def comprise_between(self, bound_my_datetime):
         return abs((bound_my_datetime.main_datetime - self.main_datetime).days)+1
+    
+    print(comprise_between)
 
     # 取得整列, 開始日期到結束日期的所有日期, 不包含結束日期
     def comprise_everyday(self, bound_my_datetime):
@@ -30,6 +32,8 @@ class MyDateTime:
             curr_datetime = self.add_day(day_counter)
             everyday_list.append(curr_datetime.date())
         return everyday_list
+
+    print(comprise_everyday)
 
     def __str__(self):
         return str(self.main_datetime)
